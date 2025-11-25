96837615ef5b420f89519388056d84e77c2eca4f
PaOneX <opcmicrosoft@gmail.com>
2025-11-25 13:03:57 +0530

    link api to project

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-  
+function sendPromt() {
+    let txtPromt= document.getElementById("txtPromt").value ;
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("X-goog-api-key", "AIzaSyDsG6-MR338Q1Pa67rT8kTIB4Je-E0I1kw");
+
+  const raw = JSON.stringify({
+    contents: [
+      {
+        parts: [
+          {
+            text: txtPromt,
+          },
+        ],
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => 
+    document.getElementById("output").innerHTML = result.candidates[0].content.parts[0].text
+    )
+    .catch((error) => console.error(error));
+}
